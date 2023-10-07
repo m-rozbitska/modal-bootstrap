d34659329ee81714033d830aa187e251f39417e6
Mariia Rozbitska <melissa180lily@gmail.com>
2023-10-07 20:18:28 +0300

    start gh page

---
+++ index.579125c3.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    const forms = document.querySelectorAll(".needs-validation");
+    Array.from(forms).forEach(function(form) {
+        form.addEventListener("submit", function(event) {
+            if (!form.checkValidity()) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+            form.classList.add("was-validated");
+        }, false);
+    });
+});
+
+//# sourceMappingURL=index.579125c3.js.map
